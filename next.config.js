4303b7aaf31b1903a5b423cdc0084e352de75442
Megumiiiiii <megumii6976@gmail.com>
2023-09-20 19:28:16 +0700

    A

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  
 })
 
 
-module.exports = withNextra({})
+module.exports = withNextra({
+  images: {
+    unoptimized: true,
+  }
+})
 
 
 
